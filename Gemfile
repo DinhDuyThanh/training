abf9177484c4193b46156ef3ac96cb126523ef04
Thanh <dinhduythanh51292@gmail.com>
2015-07-07 15:18:47 +0700

    chapter 9

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 gem 'bcrypt-ruby', '3.1.1.rc1', :require => 'bcrypt'
+gem 'faker',                '1.4.2'
 gem 'bootstrap-sass',       '3.2.0.0'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
  has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 },allow_nil: true
 def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -17,9 +17,9 @@ def User.new_token
   end
 attr_accessor :remember_token
 def remember
-    self.remember_token = ...
-    update_attribute(:remember_digest, ...)
-  end
+self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))  
+end
 def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
