59e25b6d3644eefd7d884fb14f94417ef7cabca5
Thanh <dinhduythanh51292@gmail.com>
2015-07-07 09:40:09 +0700

    create model user

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706033123) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "contect"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20150707023721) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
