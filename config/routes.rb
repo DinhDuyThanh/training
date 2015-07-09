5f3f7f25d59eeca10da98ef03c33aeccbe7a7b04
Thanh <dinhduythanh51292@gmail.com>
2015-07-09 16:03:53 +0700

    chuong 10

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
 
 
-  get 'sessions/new'
-
   root 			'static_pages#home'
 
   get 'help'	=>	'static_pages#help'
@@ -18,7 +16,11 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 resources :users  
+resources :account_activations, only: [:edit]
+resources :password_resets,     only: [:new, :create, :edit, :update]
 # The priority is based upon order of creation: first created -> highest priority.
+
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
