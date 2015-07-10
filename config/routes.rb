35c7d8c251cc66c2f58355796926e13832d70a3a
Thanh <dinhduythanh51292@gmail.com>
2015-07-10 14:25:35 +0700

    chap12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-
-
-
   root 			'static_pages#home'
 
   get 'help'	=>	'static_pages#help'
@@ -15,10 +11,16 @@ Rails.application.routes.draw do
  get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-resources :users  
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end  
 resources :account_activations, only: [:edit]
 resources :password_resets,     only: [:new, :create, :edit, :update]
 resources :microposts,		only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
 # The priority is based upon order of creation: first created -> highest priority.
 
 
